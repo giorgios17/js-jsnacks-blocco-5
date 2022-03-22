e0129ef762a3b7eb069ecd3c179e611d11ffdaae
giorgios17 <giosur@live.it>
2022-03-22 15:17:04 +0100

    fixed

--- bonus2/js/script.js
+++ bonus2/js/script.js
@@ -1,13 +1,12 @@
 //Scrivi una funzione che accetti tre argomenti: un array e due numeri (“a” più piccolo di “b” e “b” grande al massimo quanto il numero di elementi dell’array). 
 //La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra “a” e “b”
 
-const students = ['Giorgio', 'Giuseppe', 'Eleonora', 'Gianfilomenopoldo', 'Pancrazio', 'Letterio'];
+const students = ['Giorgio', 'Giuseppe', 'Eleonora', 'Gianfilomenopoldo', 'Pancrazio', 'Letterio', 'Giuanni', 'Teodorico', 'Batman'];
 
 function myFunction(array, num1, num2){
     const newArray = [];
-    const range = num2 - num1;
     if(num1 < num2 && num2 <= array.length){
-        for(let i=range; i < num2; i++){
+        for(let i=num1; i <= num2; i++){
             newArray.push(array[i]);
         }
     }
