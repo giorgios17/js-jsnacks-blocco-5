855a1d131b7d638ca6958445354dae5e2408bc5e
giorgios17 <giosur@live.it>
2022-03-22 14:54:08 +0100

    exercise 3 done

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,1 +1,13 @@
 //Scrivi una funzione che accetti una stringa come argomento e la ritorni girata (es. Ciao -> oaiC)
+
+function reverseString(string){
+    let parolaReverse= '';
+
+    for(let i=string.length -1; i >= 0; i--){
+        parolaReverse += string[i]
+    }
+
+   return parolaReverse;
+}
+
+console.log(reverseString('boolean'));
